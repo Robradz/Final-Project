1fd0284d585ffc6f6a5087a881b49c55a08beb10
Robert Radzville <rradzvil@ucsc.edu>
2021-05-13 14:00:04 -0700

    Started enemy class

---
+++ src/prefabs/Enemy.js
@@ -0,0 +1,19 @@
+class Enemy extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        this.detectionRadius = 200;
+        this.detectionDistance = 500;
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+
+    }
+}
