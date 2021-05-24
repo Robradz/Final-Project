dc98f03a1e3c82e6ef4101531c90db03c7efcaa1
z1011lu <zjlu@ucsc.edu>
2021-05-24 16:50:03 -0700

    Updated the source file location to relative

--- assets/tutorial.tsx
+++ assets/tutorial.tsx
@@ -2,72 +2,72 @@
 <tileset version="1.5" tiledversion="1.6.0" name="tutorial" tilewidth="64" tileheight="96" tilecount="23" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="1">
-  <image width="16" height="16" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/smallVent.png"/>
+  <image width="16" height="16" source=".\tileSprites/smallVent.png"/>
  </tile>
  <tile id="2">
-  <image width="32" height="32" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/tile_floor.png"/>
+  <image width="32" height="32" source=".\tileSprites/tile_floor.png"/>
  </tile>
  <tile id="4">
-  <image width="64" height="32" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/ventDuctCorner.png"/>
+  <image width="64" height="32" source=".\tileSprites/ventDuctCorner.png"/>
  </tile>
  <tile id="6">
-  <image width="64" height="64" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/wall_front.png"/>
+  <image width="64" height="64" source=".\tileSprites/wall_front.png"/>
  </tile>
  <tile id="7">
-  <image width="32" height="64" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/wall_front_innerCorner_L.png"/>
+  <image width="32" height="64" source=".\tileSprites/wall_front_innerCorner_L.png"/>
  </tile>
  <tile id="8">
-  <image width="32" height="64" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/wall_front_middle.png"/>
+  <image width="32" height="64" source=".\tileSprites/wall_front_middle.png"/>
  </tile>
  <tile id="9">
-  <image width="32" height="64" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/wall_front_outerCorner_L.png"/>
+  <image width="32" height="64" source=".\tileSprites/wall_front_outerCorner_L.png"/>
  </tile>
  <tile id="10">
-  <image width="32" height="32" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/wall_top_back.png"/>
+  <image width="32" height="32" source=".\tileSprites/wall_top_back.png"/>
  </tile>
  <tile id="11">
-  <image width="32" height="32" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/wall_top_backCorner_L.png"/>
+  <image width="32" height="32" source=".\tileSprites/wall_top_backCorner_L.png"/>
  </tile>
  <tile id="12">
-  <image width="32" height="32" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/background.png"/>
+  <image width="32" height="32" source=".\tileSprites/background.png"/>
  </tile>
  <tile id="13">
-  <image width="32" height="32" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/bigVent.png"/>
+  <image width="32" height="32" source=".\tileSprites/bigVent.png"/>
  </tile>
  <tile id="14">
-  <image width="64" height="64" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/cell_floor.png"/>
+  <image width="64" height="64" source=".\tileSprites/cell_floor.png"/>
  </tile>
  <tile id="15">
-  <image width="32" height="32" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/crate.png"/>
+  <image width="32" height="32" source=".\tileSprites/crate.png"/>
  </tile>
  <tile id="18">
-  <image width="64" height="32" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/mediumVent_middle.png"/>
+  <image width="64" height="32" source=".\tileSprites/mediumVent_middle.png"/>
  </tile>
  <tile id="19">
-  <image width="32" height="96" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/sideGate.png"/>
+  <image width="32" height="96" source=".\tileSprites/sideGate.png"/>
  </tile>
  <tile id="20">
-  <image width="64" height="32" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/ventDuctExit.png"/>
+  <image width="64" height="32" source=".\tileSprites/ventDuctExit.png"/>
  </tile>
  <tile id="21">
-  <image width="32" height="32" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/ventDuct.png"/>
+  <image width="32" height="32" source=".\tileSprites/ventDuct.png"/>
  </tile>
  <tile id="23">
-  <image width="32" height="32" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/sideVent.png"/>
+  <image width="32" height="32" source=".\tileSprites/sideVent.png"/>
  </tile>
  <tile id="24">
-  <image width="32" height="32" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/ventDuctCorner_normal.png"/>
+  <image width="32" height="32" source=".\tileSprites/ventDuctCorner_normal.png"/>
  </tile>
  <tile id="25">
-  <image width="32" height="64" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/openDoor.png"/>
+  <image width="32" height="64" source=".\tileSprites/openDoor.png"/>
  </tile>
  <tile id="26">
-  <image width="32" height="64" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/closedDoor.png"/>
+  <image width="32" height="64" source=".\tileSprites/closedDoor.png"/>
  </tile>
  <tile id="27">
-  <image width="64" height="32" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/table.png"/>
+  <image width="64" height="32" source=".\tileSprites/table.png"/>
  </tile>
  <tile id="30">
-  <image width="64" height="32" source="C:/Users/z1011/Desktop/Alien Lab Assets/PNGs/wallLight.png"/>
+  <image width="64" height="32" source=".\tileSprites/wallLight.png"/>
  </tile>
 </tileset>
