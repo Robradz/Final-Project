8ed1ff134595002756746c7a9a861959049035b9
z1011lu <zjlu@ucsc.edu>
2021-06-01 15:22:47 -0700

    Added ability sprites, worked on level 2 and 3

---
+++ assets/temp.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="temp" tilewidth="96" tileheight="96" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="96" height="96" source="tileSprites/acidTile.png"/>
+ </tile>
+</tileset>
